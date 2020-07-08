03da38529a5917312b661bd3497259a02e3afa9d
sarinb <sarinb@gmail.com>
2020-07-08 20:12:16 +0530

    Webhooklocal, validate word
    - Added webhooklocal endpoint to execute from local (without request argument)
    - Added logic for validating whether the word provided by user is a valid English word using nltk library

--- main.py
+++ main.py
@@ -13,15 +13,18 @@ def index():
     return 'Hello World!'
 
 # function for responses
-def results(res):
+def results(res, source):
     # build a request object
     req = request.get_json(force=True)
 
     # fetch action from json
     action = req.get('queryResult').get('action')
-
-    # return a fulfillment response
-    return {'fulfillment_messages': [{'text': {'text': [res]}}]}
+    if source == 'df':
+      # return formatted fulfillment response for requests from Dialogflow
+      return {'fulfillment_messages': [{'text': {'text': [res]}}]}
+    else:
+      # return normal response for other sources
+      return {'bot_word':{'word':res,'last_letter':res[-1]}}
 
 # create a route for webhook
 @app.route('/webhook', methods=['POST'])
@@ -36,18 +39,27 @@ def webhook(request):
         <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
     """
     req = request.get_json(silent=True, force=True)
+    source = ''
     try:
         action = req.get('queryResult').get('action')
+        if 'source' in req:
+          source = req.get('source')
     except AttributeError:
         return 'json error'
 
     if action == 'first':
         res = get_first_word()
-    
+ 
     print('Action: ' + action)
     print('Response: ' + res)
-    
-    return make_response(jsonify(results(res)))
+    return make_response(jsonify(results(res, source)))
+
+# Function to test the API from localhost
+@app.route('/webhooklocal', methods=['POST'])
+def webhooklocal():
+  
+  res = webhook (request)
+  return res
 
 # run the app
 if __name__ == '__main__':
